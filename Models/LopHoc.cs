5a8b4775679227e139c02ebc8bf77279b539626e
Thinh Hai <haithinh23082000@gmail.com>
2021-10-05 13:56:24 +0700

    first commit

---
+++ Models/LopHoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NguyenHaiThinh.Models
+{
+    
+    [Table("LopHoc")]
+    public class LopHoc
+    {
+        [Key]
+        public int MaLop { get; set; }
+        public string TenLop { get; set; }
+
+    }
+}
